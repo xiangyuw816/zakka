57cdb4307441052a25dc06dba4bd16be6716c4ab
xiangyuw816 <xiangyuw816@gmail.com>
2017-01-08 09:59:51 -0500

    Update lc.py

--- lc.py
+++ lc.py
@@ -53,3 +53,39 @@ def summaryRanges(self, nums):
             ranges += [],
         ranges[-1][1:] = n,
     return ['->'.join(map(str, r)) for r in ranges]
+
+# 163 missing ranges
+class Solution(object):
+    def findMissingRanges(self, nums, lower, upper):
+        """
+        :type nums: List[int]
+        :type lower: int
+        :type upper: int
+        :rtype: List[str]
+        """
+        # nums[i]-nums[i-1]==2: add nums[i]-1
+        # nums[i]-nums[i-1]>2: add nums[i-1]+1 -> nums[i]-1
+        ## use pre to present nums[i-1]
+        pre=lower-1
+        nums.append(upper+1)
+        res=[]
+        for i in nums:
+            if(i==pre+2):
+                res.append(str(i-1))
+            elif(i>pre+2):
+                res.append(str(pre+1) + '->' + str(i-1))
+            pre=i
+        return res
+    
+##    280. Wiggle Sort
+# i even: num[i]<=num[i-1]
+# i odd: num[i]>=num[i-1]
+## if i even, we have num[i-1]>=num[i-2]
+### if num[i]<=num[i-1], pass
+### if num[i]>num[i-1], swap and still have num[i-1]>=num[i-2]
+## exclusive or: (i%2) num[i]>num[i-1] - swap
+class Solution(object):
+    def wiggleSort(self, nums):
+        for i in xrange(1, len(nums)):
+            if (i % 2) ^ (nums[i] > nums[i - 1]):
+                nums[i], nums[i - 1] = nums[i - 1], nums[i]
