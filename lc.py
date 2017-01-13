88a9f4dc84690186ba9cbcc50f075018c22d0a38
xiangyuw816 <xiangyuw816@gmail.com>
2017-01-13 10:21:03 -0500

    Update lc.py

--- lc.py
+++ lc.py
@@ -607,6 +607,19 @@ class Solution(object):
         
     return True
 
-# 399.
+# 399. Evaluate Division
+## (A/B)*(B/C)*(C/D) is like the path A->B->C->D
+## use dictionary to store values
+def calcEquation(self, equations, values, queries):
+    quot = collections.defaultdict(dict)
+    # zip(equations, values): [(['a', 'b'], 2.0), (['b', 'c'], 3.0)]
+    for (num, den), val in zip(equations, values):
+        quot[num][num] = quot[den][den] = 1.0
+        quot[num][den] = val
+        quot[den][num] = 1 / val
+    for k, i, j in itertools.permutations(quot, 3):
+        if k in quot[i] and j in quot[k]:
+            quot[i][j] = quot[i][k] * quot[k][j]
+    return [quot[num].get(den, -1.0) for num, den in queries]
 
 # 165. Compare Version Numbers
