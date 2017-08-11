69d53e9e332c4bd4f6b10e1855ca389c5591fab8
xiangyuw816 <xiangyuw816@gmail.com>
2017-08-10 23:51:54 -0400

    Update lc2.py

--- lc2.py
+++ lc2.py
@@ -43,3 +43,67 @@ def removeElement(self, nums, val):
                 nums[nextPos] = num
                 nextPos += 1
         return nextPos
+
+"""118. Pascal's Triangle"""
+# actually 1st and last elemenet of each row are both 1
+# res.append([1]*(i+1))
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        if numRows == 0:
+            return []
+        res = [[1]]
+        for i in range(1, numRows):
+            res.append([])
+            for j in range(i+1):
+                res[i].append((res[i-1][j-1] if j>0 else 0) + (res[i-1][j] if j<i else 0))
+        return res
+      
+"""242. Valid Anagram"""
+# sort two string and compare: sorted(s) == sorted(t)
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+
+        alpha = {}
+        beta = {}
+        for c in s:
+            alpha[c] = alpha.get(c, 0) + 1
+        for c in t:
+            beta[c] = beta.get(c, 0) + 1
+        return alpha == beta
+      
+"""463. Island Perimeter"""
+# count different 1/0
+class Solution(object):
+    def islandPerimeter(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        if not grid:
+            return 0
+
+        def sum_adjacent(i, j):
+            adjacent = (i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1),
+            res = 0
+            for x, y in adjacent:
+                if x < 0 or y < 0 or x == len(grid) or y == len(grid[0]) or grid[x][y] == 0:
+                    res += 1
+            return res
+
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    count += sum_adjacent(i, j)
+        return count
