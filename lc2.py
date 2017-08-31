9c808fd15b04baa6eafb19f13fd699747060df6c
xiangyuw816 <xiangyuw816@gmail.com>
2017-08-31 09:38:30 -0700

    Update lc2.py

--- lc2.py
+++ lc2.py
@@ -875,3 +875,54 @@ def solution(a1,a2,a3):
             res.extend([term]*t)
     
     return res
+
+ 
+"""133. Clone Graph"""
+class Solution:
+    def cloneGraph(self, node):
+        root = node
+        if node is None:
+            return node
+            
+        # BFS to traverse the graph and get all nodes.
+        nodes = self.getNodes(node)
+        
+        # copy nodes, store the old->new mapping information in a hash map
+        mapping = {}
+        for node in nodes:
+            mapping[node] = UndirectedGraphNode(node.label)
+        
+        # copy neighbors(edges)
+        for node in nodes:
+            new_node = mapping[node]
+            for neighbor in node.neighbors:
+                new_neighbor = mapping[neighbor]
+                new_node.neighbors.append(new_neighbor)
+        
+        return mapping[root]
+        
+    def getNodes(self, node):
+        q = collections.deque([node])
+        result = set([node])
+        while q:
+            head = q.popleft()
+            for neighbor in head.neighbors:
+                if neighbor not in result: # prune
+                    result.add(neighbor)
+                    q.append(neighbor)
+        return result
+
+"""238. Product of Array Except Self"""      
+def productExceptSelf(nums):
+    # times twice: 1st - times every element before and then times every element after
+    p = 1
+    n = len(nums)
+    output = []
+    for i in range(n):
+        output.append(p)
+        p *= nums[i]
+    p = 1
+    for i in range(n-1,-1,-1):
+        output[i] = output[i] * p
+        p *= nums[i]
+    return output
