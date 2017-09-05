1cdf5b37cf9c94cd4c7470043ce0da8a1c6e2eb8
xiangyuw816 <xiangyuw816@gmail.com>
2017-09-05 19:40:53 -0400

    Update lc2.py

--- lc2.py
+++ lc2.py
@@ -1020,3 +1020,21 @@ def calculate(s):
             sign = s[i]
             num = 0
     return sum(stack)
+
+"""Binary Tree Maximum Path Sum"""
+# two situations: 1. node.val + one side; 2. node.val + 2 sides
+class Solution(object):
+    def maxPathSum(self, root):
+        def dfs(node):  # returns: max one side path sum, max path sum
+            l = r = 0
+            ls = rs = None
+            if node.left:
+                l, ls = dfs(node.left)
+                l = max(l, 0)
+            if node.right:
+                r, rs = dfs(node.right)
+                r = max(r, 0)
+            return node.val + max(l, r), max(node.val + l + r, ls, rs)
+        if root:
+            return dfs(root)[1]
+        return 0
