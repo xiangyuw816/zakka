3fd4266bb2008fdef40c32864598f93edc7ccfcd
xiangyuw816 <xiangyuw816@gmail.com>
2017-09-10 12:48:03 -0400

    Update lc2.py

--- lc2.py
+++ lc2.py
@@ -1087,3 +1087,18 @@ def isOneEditDistance(s, t):
   
 # when it's about group together:
 # one way is to use hashtap and then return dict.values().
+
+
+"""88. Merge Sorted Array"""
+# sort from the end to beginning
+# use m,n as ct ~ how many un-sorted left
+def merge(self, nums1, m, nums2, n):
+        while m > 0 and n > 0:
+            if nums1[m-1] >= nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[m+n-1] = nums2[n-1]
+                n -= 1
+        if n > 0:
+            nums1[:n] = nums2[:n]
