141ff2fb908d353ea099e2b4c39ddfa3de2ebaa3
xiangyuw816 <xiangyuw816@gmail.com>
2017-09-10 20:13:05 -0400

    Update lc3.py

--- lc3.py
+++ lc3.py
@@ -25,3 +25,18 @@ class solution():
                     return True
 
         return False
+
+    
+"""Maximum Size Subarray Sum Equals k"""
+# continuous sub-array
+def maxSubArrayLen(nums, k):
+    ans, acc = 0, 0               # answer and the accumulative value of nums
+    mp = {0:-1}                 #key is acc value, and value is the index
+    for i in xrange(len(nums)):
+        acc += nums[i]
+        if acc not in mp:
+            mp[acc] = i
+        if acc-k in mp:
+            print(i, mp, i-mp[acc-k])
+            ans = max(ans, i-mp[acc-k])
+    return ans
