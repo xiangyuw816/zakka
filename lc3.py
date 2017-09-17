c8fda6d04021c85cf40f407e4a896d62b2eb97b5
xiangyuw816 <xiangyuw816@gmail.com>
2017-09-16 23:34:05 -0400

    Update lc3.py

--- lc3.py
+++ lc3.py
@@ -345,3 +345,25 @@ def get_min(root):
         root = root.left
         m = root.val
     return m
+
+
+"""2. Add Two Numbers"""
+# Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+# Output: 7 -> 0 -> 8
+# i.e. 342 + 465 = 708
+def addTwoNumbers(self, l1, l2):
+    carry = 0
+    root = n = ListNode(0)
+    while l1 or l2 or carry:
+        v1 = v2 = 0 # fillna
+        if l1:
+            v1 = l1.val
+            l1 = l1.next
+        if l2:
+            v2 = l2.val
+            l2 = l2.next
+        carry, val = divmod(v1+v2+carry, 10)
+        n.next = ListNode(val)
+        n = n.next
+        
+    return root.next
