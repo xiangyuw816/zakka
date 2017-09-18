08221af612f6f118becc9cd81da80446e707cd86
xiangyuw816 <xiangyuw816@gmail.com>
2017-09-17 22:01:57 -0400

    Update lc3.py

--- lc3.py
+++ lc3.py
@@ -422,3 +422,21 @@ class Solution(object):
                 r = mid
         return arr[l:l+k]
     
+
+"""360. Sort Transformed Array"""
+def sortTransformedArray(self, nums, a, b, c):
+    # two pointers to traverse from two ends
+    # if a>0: the max should be from two ends (half sorted)
+    nums = [x*x*a + x*b + c for x in nums]
+    ret = [0] * len(nums)
+    p1, p2 = 0, len(nums) - 1
+    i, step = (p1, 1) if a < 0 else (p2, -1)
+    while p1 <= p2:
+        if nums[p1] * -step > nums[p2] * -step:
+            ret[i] = nums[p1]
+            p1 += 1
+        else:
+            ret[i] = nums[p2]
+            p2 -=1
+        i += step
+    return ret
